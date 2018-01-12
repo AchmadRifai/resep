2d8c0e27bad1c324697f234ce8df031cf697443b
Achmad Rifa'i <achmad.rifai.jowo.asli@gmail.com>
2018-01-12 14:14:27 +0700

    yo
    yo

--- src/Conf.js
+++ src/Conf.js
@@ -6,28 +6,26 @@ class Conf extends Component {
     constructor(props){
         super(props);
         this.error={data:''};
-        this.conf={host:'localhost',dbname:'',port:0,user:'root',pass:''};
+        this.conf={host:'localhost',dbname:'',port:3306,user:'root',pass:''};
     }
 
     jalan(e){
         var fs=require('fs');
         if(1000<=this.conf.port&&''!==this.conf.dbname&&''!==this.conf.host&&''!==this.conf.user&&''!==this.conf.pass){
-            fs.writeFile('dbkonf',JSON.stringify(this.conf),function(e){
-                if(!e){
-                    var mysql=require('mysql');
-                    var ene=null;
-                    var c=mysql.createConnection({host:this.conf.host,port:this.conf.port,database:this.conf.dbname,user:this.conf.user,password:this.conf.pass});
-                    c.connect();
-                    c.query('create table resep(nama varchar(25)primary key,jenis varchar(10)not null,gbr text not null,ket text not null)',function(e,r,f){if(e)ene=e;});
-                    c.query('create table bahan(resep varchar(25)not null,nama varchar(20)not null,jum float not null,satuan varchar(20)not null)',function(e,r,f){if(e)ene=e;});
-                    c.query('create table langkah(resep varchar(25)not null,urut int not null,stepe text not null)',function(e,r,f){if(e)ene=e;});
-                    c.query('alter table bahan add foreign key(resep)references resep(kode)on update cascade on delete cascade',function(e,r,f){if(e)ene=e;});
-                    c.query('alter table langkah add foreign key(resep)references resep(kode)on update cascade on delete cascade',function(e,r,f){if(e)ene=e;});
-                    c.end();
-                    if(!ene)window.location.href='index.html';
-                }
+            var mysql=require('mysql');
+            var c=mysql.createConnection({host:this.conf.host,port:this.conf.port,database:this.conf.dbname,user:this.conf.user,password:this.conf.pass});
+            c.connect();
+            c.query('create table if not exists resep(nama varchar(25)primary key,jenis varchar(10)not null,gbr text not null,ket text not null)',
+            function(e,r,f){});
+            c.query('create table if not exists bahan(resep varchar(25)not null,nama varchar(20)not null,jum float not null,satuan varchar(20)not null)',
+            function(e,r,f){
+                c.query('alter table bahan add foreign key(resep)references resep(kode)on update cascade on delete cascade',function(e,r,f){});
             });
-        }
+            c.query('create table langkah(resep varchar(25)not null,urut int not null,stepe text not null)',
+            function(e,r,f){
+                c.query('alter table langkah add foreign key(resep)references resep(kode)on update cascade on delete cascade',function(e,r,f){});
+            });c.end();
+        }else alert('Tolong Diisi Semua!');
     }
 
     render() {
@@ -39,10 +37,11 @@ class Conf extends Component {
             </Header>
             <Form size='large'>
                 <Segment stacked>
-                    <Form.Input fluid icon='desktop' iconPosition='left' placeholder='Host' value={this.conf.host}/>
-                    <Form.Input fluid icon='plug' iconPosition='left' placeholder='Port' value={this.conf.port}/>
-                    <Form.Input fluid icon='database' iconPosition='left' placeholder='Nama Database' value={this.conf.dbname}/>
-                    <Form.Input fluid icon='user' iconPosition='left' placeholder='Host' value={this.conf.user}/>
+                    <Form.Input fluid icon='desktop' iconPosition='left' placeholder='Host *' value={this.conf.host} type="text"/>
+                    <Form.Input fluid icon='plug' iconPosition='left' placeholder='Port *' value={this.conf.port} type="text"/>
+                    <Form.Input fluid icon='database' iconPosition='left' placeholder='Nama Database *' value={this.conf.dbname} type="text"/>
+                    <Form.Input fluid icon='user' iconPosition='left' placeholder='Host *' value={this.conf.user} type="text"/>
+                    <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password' value={this.conf.pass} type="password"/>
                     <Button color='teal' fluid size='large' onClick={this.jalan}>Login</Button>
                 </Segment>
             </Form>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Conf from './Conf';
 import 'semantic-ui-css/semantic.min.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Conf />, document.getElementById('root'));
 registerServiceWorker();
